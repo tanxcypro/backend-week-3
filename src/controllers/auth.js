7a5e130d8c86c9cf55547dbcb1c9a012b59b223b
tanxcypro <58644969+tanxcypro@users.noreply.github.com>
2022-05-30 22:41:13 +0700

    update

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,6 +15,7 @@ exports.register = async (req, res) => {
     name: Joi.string().min(5).required(),
     email: Joi.string().email().min(6).required(),
     password: Joi.string().min(6).required(),
+    status: Joi.string().required(),
   });
 
   // do validation and get error object from schema.validate
